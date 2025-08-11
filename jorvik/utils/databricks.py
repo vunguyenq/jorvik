be4c6b56404d1dbe07fd7fbe2da42ba7c303a21a
vunguyenq <vu.nguyenq89@gmail.com>
2025-08-11 22:47:50 +0200

    added path_action param

--- jorvik/utils/databricks.py
+++ jorvik/utils/databricks.py
@@ -1,8 +1,9 @@
 '''This module provides utility functions to interact with Databricks built in utilities (default spark context, dbutils).'''
 import json
-from typing import Any
+from typing import Any, List
 
 from pyspark.sql import SparkSession
+from pyspark.errors.exceptions.captured import AnalysisException
 
 
 class DatabricksUtilsError(Exception):
@@ -77,3 +78,51 @@ def get_cluster_id() -> str:
 def get_notebook_path() -> str:
     """ Gets the current notebook path"""
     return get_notebook_context()["extraContext"]["notebook_path"]
+
+def sanitize_dbfs_path(path: str) -> str:
+    """ Sanitizes a DBFS path by removing the 'dbfs:' and/ or 'mnt' prefix if present."""
+    if path.startswith("dbfs:"):
+        path = path[4:]
+    if path.startswith("mnt"):
+        path = path[3:]
+    return path
+
+def scan_delta_tables(dir_path: str, max_level: int = 5,
+                      level: int = 0, exclude_paths: List[str] = []) -> List[str]:
+    """ Recursively scans a directory and detect delta tables in subdirectories.
+    Args:
+        dir_path (str): The directory path to scan.
+        max_level (int): Maximum depth of subdirectory scanning. Default is 5.
+        level (int): Current recursion depth.
+        exclude_paths (List[str]): List of paths to exclude from scanning.
+    Returns:
+        List[str]: List of detected delta table paths.
+    """
+    spark = get_spark()
+    dbutils = get_dbutils()
+
+    # If no delta table is detected after max_level, abandon the path
+    if level > max_level:
+        return []
+
+    # Stop scanning if the directory is in the exclude list
+    if sanitize_dbfs_path(dir_path) in [sanitize_dbfs_path(p) for p in exclude_paths]:
+        return []
+
+    # Check if current path is a delta table
+    try:
+        df = spark.read.format("delta").load(dir_path)
+        if df is not None:
+            return [dir_path]
+    except AnalysisException:
+        pass  # Not a delta table, continue scanning
+    except Exception:  # Detected as delta table but failed to read, possibly due to corruption.
+        return []
+
+    delta_paths = []
+    subdirs = [f.path for f in dbutils.fs.ls(dir_path) if f.isDir()]
+
+    for p in subdirs:
+        delta_paths.extend(scan_delta_tables(p, max_level, level + 1, exclude_paths))
+
+    return delta_paths
